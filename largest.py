c503cb4c46e900d4ba602971981b14d3c7c7d527
Tomás Proaño <67541868+tomasproanop@users.noreply.github.com>
2023-07-02 15:42:55 +0200

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,7 +1,7 @@
 largest_so_far = -1
-print('Before', largest_so_far)
-for the_num in [9, 41, 12, 3, 74, 15] :
-    if the_num > largest_so_far :
-        largest_so_far = the_num
-    print(largest_so_far, the_num)
-print('After', largest_so_far)
+print('Largest number before:', largest_so_far)
+for number in [9, 41, 12, 3, 74, 15] :
+    if number > largest_so_far :
+        largest_so_far = number
+    print(largest_so_far, number)
+print('Largest Number afterwards:', largest_so_far)
